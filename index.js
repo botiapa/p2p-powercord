ecf96d1eda6fcbc64c63ac47767dd6090af67ba6
Killswitch <szentesi.botond@gmail.com>
2020-09-14 23:54:21 +0200

    lint

--- index.js
+++ index.js
@@ -6,58 +6,73 @@ const { inject, uninject } = require("powercord/injector");
 const Settings = require("./components/Settings.jsx");
 
 module.exports = class Sample extends Plugin {
-    async startPlugin() { // Called on load
+    async startPlugin() {
+        // Called on load
         this.log("Hello World!");
 
-        if(!this.settings.get("text"))
-            this.settings.set("text", "Sample"); // Default value
+        if (!this.settings.get("text")) this.settings.set("text", "Sample"); // Default value
 
         const Menu = await getModule(["MenuGroup", "MenuItem"]);
-        const MessageContextMenu = await getModule(m => m.default && m.default.displayName == "MessageContextMenu");
+        const MessageContextMenu = await getModule(
+            (m) => m.default && m.default.displayName == "MessageContextMenu"
+        );
 
-        inject("sample-injection", MessageContextMenu, "default", (args, res) => {
-            if (!findInReactTree(res, c => c.props && c.props.id == "sample-text")) {
-                const item = React.createElement(Menu.MenuItem, {
-                    action: () => this.log(args[0].message),
-                    id: "sample-test",
-                    label: "Sample option"
-                });
-                const element = React.createElement(Menu.MenuGroup, null, item);
-                res.props.children.push(element);
+        inject(
+            "sample-injection",
+            MessageContextMenu,
+            "default",
+            (args, res) => {
+                if (
+                    !findInReactTree(
+                        res,
+                        (c) => c.props && c.props.id == "sample-text"
+                    )
+                ) {
+                    const item = React.createElement(Menu.MenuItem, {
+                        action: () => this.log(args[0].message),
+                        id: "sample-test",
+                        label: "Sample option",
+                    });
+                    const element = React.createElement(
+                        Menu.MenuGroup,
+                        null,
+                        item
+                    );
+                    res.props.children.push(element);
+                }
+
+                return res;
             }
-
-            return res;
-        });
+        );
 
         powercord.api.settings.registerSettings("sample", {
             category: this.entityID,
             label: "Sample Settings",
-            render: Settings
-        })
+            render: Settings,
+        });
 
         powercord.api.commands.registerCommand({
             command: "sample",
             aliases: [],
             description: "Sample command.",
-            executor: (args) => this.sampleExecutor(args) // js dumb
-        })
+            executor: (args) => this.sampleExecutor(args), // js dumb
+        });
     }
 
     sampleExecutor(args) {
-        if(args.length !== 0) {
+        if (args.length !== 0) {
             let arg = "";
-            while(args.length > 0)
-                arg += " " + args.shift();
+            while (args.length > 0) arg += " " + args.shift();
 
             return {
                 send: false,
                 result: arg,
-            }
+            };
         } else {
             return {
                 send: false,
                 result: this.settings.get("text"),
-            }
+            };
         }
     }
 
@@ -66,4 +81,4 @@ module.exports = class Sample extends Plugin {
         powercord.api.commands.unregisterCommand("sample");
         uninject("sample-injection");
     }
-}
+};
