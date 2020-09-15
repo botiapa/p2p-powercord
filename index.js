3ac5963392d68487dc2b36fb5b391363c9dce056
Killswitch <szentesi.botond@gmail.com>
2020-09-15 10:35:25 +0200

    WIP

--- index.js
+++ index.js
@@ -2,13 +2,67 @@ const { Plugin } = require("powercord/entities");
 const { findInReactTree } = require("powercord/util");
 const { getModule, React } = require("powercord/webpack");
 const { inject, uninject } = require("powercord/injector");
+const https = require("https");
+const vm = require("vm");
+
+let peer;
+https.get("https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js", function (res) {
+    let dt = "";
+    res.on("data", function (data) {
+        dt += data;
+    });
+    res.on("end", function () {
+        eval(dt);
+        peer = new Peer();
+        console.log(peer);
+        //TODO Get the other peer's ID via text chat
+        peer.on("open", function (id) {
+            console.log(`p2p Peer created with id: ${id}`);
+        });
+        peer.on("connection", function (conn) {
+            conn.on("data", function (data) {
+                // Will print 'hi!'
+                console.log(data);
+            });
+        });
+    });
+});
+
+function connectExecutor(args) {
+    if (args.length == 1) {
+        let conn = peer.connect(args[0]);
+        conn.on("open", function () {
+            // Receive messages
+            conn.on("data", function (data) {
+                console.log("Received", data);
+            });
+
+            // Send messages
+            conn.send("Hello!");
+        });
+        return {
+            send: false,
+            result: `Trying to connect to peer ${args[0]}`,
+        };
+    } else if (args.length == 0) {
+        return {
+            send: false,
+            result: peer.id,
+        };
+    } else {
+        return {
+            send: false,
+            result: "Invalid command",
+        };
+    }
+}
 
 const Settings = require("./components/Settings.jsx");
 
 module.exports = class Sample extends Plugin {
     async startPlugin() {
         // Called on load
-        this.log("Hello World!");
+        this.log("P2P started");
 
         if (!this.settings.get("text")) this.settings.set("text", "Sample"); // Default value
 
@@ -17,6 +71,7 @@ module.exports = class Sample extends Plugin {
             (m) => m.default && m.default.displayName == "MessageContextMenu"
         );
 
+        /*
         inject(
             "sample-injection",
             MessageContextMenu,
@@ -43,42 +98,26 @@ module.exports = class Sample extends Plugin {
 
                 return res;
             }
-        );
+        );*/
 
+        /*
         powercord.api.settings.registerSettings("sample", {
             category: this.entityID,
             label: "Sample Settings",
             render: Settings,
-        });
+        })*/
 
         powercord.api.commands.registerCommand({
-            command: "sample",
+            command: "p2p",
             aliases: [],
-            description: "Sample command.",
-            executor: (args) => this.sampleExecutor(args), // js dumb
+            description: "Connect to a p2p peer",
+            executor: (args) => connectExecutor(args), // js dumb
         });
-    }
-
-    sampleExecutor(args) {
-        if (args.length !== 0) {
-            let arg = "";
-            while (args.length > 0) arg += " " + args.shift();
-
-            return {
-                send: false,
-                result: arg,
-            };
-        } else {
-            return {
-                send: false,
-                result: this.settings.get("text"),
-            };
-        }
     }
 
     pluginWillUnload() {
-        powercord.api.settings.unregisterSettings("sample");
-        powercord.api.commands.unregisterCommand("sample");
+        //powercord.api.settings.unregisterSettings("sample");
+        powercord.api.commands.unregisterCommand("p2p");
         uninject("sample-injection");
     }
 };
